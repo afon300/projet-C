30b0301492b37a9b332f537415a67813670c5534
afon30 <antoinefourneyron@gmail.com>
2024-05-20 03:14:02 +0200

    petites modifications d'organisation

--- cdataframe.c
+++ cdataframe.c
@@ -1,5 +1,6 @@
 #include "cdataframe.h"
 #include "column.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- column.c
+++ column.c
@@ -1,9 +1,9 @@
-#include "column.h"
-#include "fonctions.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "column.h"
+#include "fonctions.h"
 
 #define INIT_SIZE 255
 #define REALLOC_SIZE 255
